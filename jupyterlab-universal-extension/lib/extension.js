71321696ffc83810ad2c2336ec466a9bf38cab1c
rickmcgeer <rick@mcgeer.com>
2021-03-03 20:38:36 -0800

    Added .gd files to the extension

--- jupyterlab-universal-extension/lib/extension.js
+++ jupyterlab-universal-extension/lib/extension.js
@@ -1,5 +1,14 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const application_1 = require("@jupyterlab/application");
 const docmanager_1 = require("@jupyterlab/docmanager");
@@ -10,6 +19,17 @@ const rendermime_1 = require("@jupyterlab/rendermime");
 const toc_1 = require("./toc");
 require("../style/index.css");
 const widgets_1 = require("@lumino/widgets");
+const apputils_1 = require("@jupyterlab/apputils");
+const launcher_1 = require("@jupyterlab/launcher");
+const filebrowser_1 = require("@jupyterlab/filebrowser");
+const mainmenu_1 = require("@jupyterlab/mainmenu");
+const FACTORY = 'Editor';
+const ICON_CLASS = 'jp-PythonIcon';
+const PALETTE_CATEGORY = 'Text Editor';
+var CommandIDs;
+(function (CommandIDs) {
+    CommandIDs.createNew = 'fileeditor:create-new-galyleo-file';
+})(CommandIDs || (CommandIDs = {}));
 /**
  * Activates the ToC extension.
  *
@@ -24,14 +44,63 @@ const widgets_1 = require("@lumino/widgets");
  * @param rendermime - rendered MIME registry
  * @returns table of contents registry
  */
-function activateTOC(app, docmanager, editorTracker, labShell, restorer, markdownViewerTracker, notebookTracker, rendermime) {
+function activateTOC(app, docmanager, editorTracker, labShell, restorer, markdownViewerTracker, notebookTracker, rendermime, browserFactory, launcher, menu, palette) {
     const editor = new toc_1.GalyleoEditor({ docmanager, notebook: notebookTracker, labShell, app });
     editor.title.iconClass = 'jp-TableOfContents-icon jp-SideBar-tabIcon';
     editor.title.caption = 'Galyleo Editor';
-    editor.id = 'table-of-contents';
+    editor.id = 'Galyleo Editor';
     labShell.add(editor, 'right', { rank: 700 });
-    restorer.add(editor, 'juputerlab-toc');
+    restorer.add(editor, 'jupyterlab-toc');
     widgets_1.BoxPanel.setStretch(editor.parent, 2);
+    const { commands, contextMenu } = app;
+    const command = CommandIDs.createNew;
+    commands.addCommand(command, {
+        label: args => args['isPalette'] || args['isContextMenu']
+            ? 'New Galyleo Dashboard'
+            : 'Galyleo Dashboard',
+        caption: 'Create a new Galyleo Dashboard',
+        iconClass: args => (args['isPalette'] ? '' : ICON_CLASS),
+        execute: (args) => __awaiter(this, void 0, void 0, function* () {
+            var _a, _b;
+            const cwd = (_b = (_a = args['cwd']) !== null && _a !== void 0 ? _a : browserFactory === null || browserFactory === void 0 ? void 0 : browserFactory.defaultBrowser.model.path) !== null && _b !== void 0 ? _b : undefined;
+            const model = yield commands.execute('docmanager:new-untitled', {
+                path: cwd,
+                type: 'file',
+                ext: 'gd'
+            });
+            return commands.execute('docmanager:open', {
+                path: model.path,
+                factory: FACTORY
+            });
+        })
+    });
+    const selectorContent = '.jp-DirListing-content';
+    contextMenu.addItem({
+        command,
+        args: { isContextMenu: true },
+        selector: selectorContent,
+        rank: 3
+    });
+    // add to the launcher
+    if (launcher) {
+        launcher.add({
+            command,
+            category: 'Other',
+            rank: 1
+        });
+    }
+    // add to the palette
+    if (palette) {
+        palette.addItem({
+            command,
+            args: { isPalette: true },
+            category: PALETTE_CATEGORY
+        });
+    }
+    // add to the menu
+    if (menu) {
+        menu.fileMenu.newMenu.addGroup([{ command }], 30);
+    }
     return editor;
 }
 /**
@@ -49,7 +118,11 @@ const extension = {
         application_1.ILayoutRestorer,
         markdownviewer_1.IMarkdownViewerTracker,
         notebook_1.INotebookTracker,
-        rendermime_1.IRenderMimeRegistry
+        rendermime_1.IRenderMimeRegistry,
+        filebrowser_1.IFileBrowserFactory,
+        launcher_1.ILauncher,
+        mainmenu_1.IMainMenu,
+        apputils_1.ICommandPalette
     ],
     activate: activateTOC
 };
--- jupyterlab-universal-extension/src/extension.ts
+++ jupyterlab-universal-extension/src/extension.ts
@@ -15,6 +15,18 @@ import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
 import { GalyleoEditor } from './toc';
 import '../style/index.css';
 import { BoxPanel, Widget } from '@lumino/widgets';
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
+import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
+const FACTORY = 'Editor';
+const ICON_CLASS = 'jp-PythonIcon';
+const PALETTE_CATEGORY = 'Text Editor';
+
+namespace CommandIDs {
+  export const createNew = 'fileeditor:create-new-galyleo-file';
+}
 
 /**
  * Activates the ToC extension.
@@ -39,15 +51,73 @@ function activateTOC(
   restorer: ILayoutRestorer,
   markdownViewerTracker: IMarkdownViewerTracker,
   notebookTracker: INotebookTracker,
-  rendermime: IRenderMimeRegistry
+  rendermime: IRenderMimeRegistry,
+  browserFactory: IFileBrowserFactory | null,
+  launcher: ILauncher | null,
+  menu: IMainMenu | null,
+  palette: ICommandPalette
 ): GalyleoEditor {
   const editor = new GalyleoEditor({ docmanager, notebook: notebookTracker, labShell, app });
   editor.title.iconClass = 'jp-TableOfContents-icon jp-SideBar-tabIcon';
   editor.title.caption = 'Galyleo Editor';
-  editor.id = 'table-of-contents';
+  editor.id = 'Galyleo Editor';
   labShell.add(editor, 'right', { rank: 700 });  
-  restorer.add(editor, 'juputerlab-toc');
+  restorer.add(editor, 'jupyterlab-toc');
   BoxPanel.setStretch(editor.parent as Widget, 2);
+  const { commands, contextMenu } = app;
+  const command = CommandIDs.createNew;
+  commands.addCommand(command, {
+    label: args =>
+      args['isPalette'] || args['isContextMenu']
+        ? 'New Galyleo Dashboard'
+        : 'Galyleo Dashboard',
+    caption: 'Create a new Galyleo Dashboard',
+    iconClass: args => (args['isPalette'] ? '' : ICON_CLASS),
+    execute: async args => {
+      const cwd =
+        args['cwd'] ?? browserFactory?.defaultBrowser.model.path ?? undefined;
+      const model = await commands.execute('docmanager:new-untitled', {
+        path: cwd,
+        type: 'file',
+        ext: 'gd'
+      });
+      return commands.execute('docmanager:open', {
+        path: model.path,
+        factory: FACTORY
+      });
+    }
+  });
+  const selectorContent = '.jp-DirListing-content';
+  contextMenu.addItem({
+    command,
+    args: { isContextMenu: true },
+    selector: selectorContent,
+    rank: 3
+  });
+
+  // add to the launcher
+  if (launcher) {
+    launcher.add({
+      command,
+      category: 'Other',
+      rank: 1
+    });
+  }
+
+  // add to the palette
+  if (palette) {
+    palette.addItem({
+      command,
+      args: { isPalette: true },
+      category: PALETTE_CATEGORY
+    });
+  }
+
+  // add to the menu
+  if (menu) {
+    menu.fileMenu.newMenu.addGroup([{ command }], 30);
+  }
+
   return editor;
 }
 
@@ -66,7 +136,11 @@ const extension: JupyterFrontEndPlugin<GalyleoEditor> = {
     ILayoutRestorer,
     IMarkdownViewerTracker,
     INotebookTracker,
-    IRenderMimeRegistry
+    IRenderMimeRegistry,
+    IFileBrowserFactory,
+    ILauncher,
+    IMainMenu,
+    ICommandPalette
   ],
   activate: activateTOC
 };
