a1202c168cc0513a82d82cce73265e22dadb09dc
rickmcgeer <rick@mcgeer.com>
2021-04-19 15:30:39 -0700

    added user to the URL in _render.  Note that we are assuming email addresses as userids; looking for the component with an @

--- jupyterlab-universal-extension/src/editor.ts
+++ jupyterlab-universal-extension/src/editor.ts
@@ -5,12 +5,12 @@ import { Widget } from '@phosphor/widgets';
 import { DocumentRegistry, DocumentWidget } from '@jupyterlab/docregistry';
 import { GalyleoModel } from './extension';
 
-export class GalyleoDocument extends DocumentWidget<GalyleoEditor, GalyleoModel> {
-
-}
+export class GalyleoDocument extends DocumentWidget<
+  GalyleoEditor,
+  GalyleoModel
+> {}
 
 export class GalyleoEditor extends Widget {
-
   private _iframe: HTMLIFrameElement;
   private _context: DocumentRegistry.IContext<GalyleoModel>;
   private _completeSave: Function;
@@ -21,51 +21,70 @@ export class GalyleoEditor extends Widget {
     this._iframe = document.createElement('iframe');
     this._iframe.style.cssText = 'width: 100%; height: 100%; border: 0px;';
     this.node.appendChild(this._iframe);
-    this.node.onmouseleave = () => this._iframe.style.pointerEvents = 'none';
+    this.node.onmouseleave = () => (this._iframe.style.pointerEvents = 'none');
     this.node.onmousemove = (evt: MouseEvent) => {
-      if (document.getElementsByClassName('lm-Menu-content').length == 0) this._iframe.style.pointerEvents = 'auto';
-    }
+      if (document.getElementsByClassName('lm-Menu-content').length == 0)
+        this._iframe.style.pointerEvents = 'auto';
+    };
     void this._context.ready.then(async () => {
       await this._render();
-  });
+    });
   }
 
   onAfterShow() {
     // fix the labes in the scene that have bee update while hidden
-    this._iframe.contentWindow?.postMessage({ method: 'galyleo:fixLabels' }, "*");
+    this._iframe.contentWindow?.postMessage(
+      { method: 'galyleo:fixLabels' },
+      '*'
+    );
   }
 
-  get editor() { return this }
+  get editor() {
+    return this;
+  }
 
   completeSave() {
     this._completeSave(true);
   }
 
   async requestSave(path: string) {
-    this._iframe.contentWindow?.postMessage({ method: 'galyleo:save', path }, "*");
-    await new Promise((resolve) => this._completeSave = resolve);
+    this._iframe.contentWindow?.postMessage(
+      { method: 'galyleo:save', path },
+      '*'
+    );
+    await new Promise(resolve => (this._completeSave = resolve));
   }
 
   loadDashboard(jsonString: string) {
-    this._iframe.contentWindow?.postMessage({ method: 'galyleo:load', jsonString }, "*");
+    this._iframe.contentWindow?.postMessage(
+      { method: 'galyleo:load', jsonString },
+      '*'
+    );
   }
 
   // we are the receivers of the undo/redo commands
 
   undo() {
-    this._iframe.contentWindow?.postMessage({ method: 'galyleo:undo'}, '*');
+    this._iframe.contentWindow?.postMessage({ method: 'galyleo:undo' }, '*');
   }
 
   redo() {
-    this._iframe.contentWindow?.postMessage({ method: 'galyleo:redo'}, '*');
+    this._iframe.contentWindow?.postMessage({ method: 'galyleo:redo' }, '*');
   }
 
   async _render() {
     // now set the src accordingly on the iframe....?
-    const filePath = this._context.path
+    const filePath = this._context.path;
     const sessionId = this._context.model.session;
-    const baseURL = 'https://matt.engagelively.com/users/rick/published/Dashboard%20Studio%20Development/index.html';
-    this._iframe.src = `${baseURL}?dashboard_file=${filePath}&session=${sessionId}&inJupyterLab=true`;
+    // dig out the user from the URL; it will be the component of the path with an @ in it
+    const parentUrl = window.location.href;
+    const components = parentUrl.split('/');
+    const userComponents = components.filter(comp => comp.indexOf('@') >= 0);
+    const user = userComponents.length > 0 ? userComponents[0] : '';
+    // assemble the url and load it into the iframe
+    const baseURL =
+      'https://matt.engagelively.com/users/rick/published/Dashboard%20Studio%20Development/index.html;';
+    this._iframe.src = `${baseURL}?dashboard_file=${filePath}&session=${sessionId}&inJupyterLab=true&user=${user}`;
     // wait for session to load
   }
 }
