8042d529d6ad8fcfb98d001f80812bc016516747
Robin Schreiber <robin.schreiber@me.com>
2022-01-26 15:59:51 +0100

    search: add drop shadow

--- studio/inputs/search.cp.js
+++ studio/inputs/search.cp.js
@@ -1,13 +1,13 @@
 import { InputLineDefault } from 'lively.components/inputs.cp.js';
 import { component } from 'lively.morphic/components/core.js';
 import { ShadowObject } from 'lively.morphic';
-import { pt } from 'lively.graphics';
+import { pt, Color } from 'lively.graphics';
 
 // GalyleoSearch.openInWorld()
 const GalyleoSearch = component(InputLineDefault, {
   name: 'galyleo search',
   borderRadius: 50,
-  dropShadow: new ShadowObject({ distance: 0, blur: 0 }),
+  dropShadow: new ShadowObject({ rotation: 90, color: Color.rgba(102, 102, 102, 0.65), blur: 8 }),
   extent: pt(318.1, 28.8),
   fontFamily: 'Barlow',
   fontSize: 15,
