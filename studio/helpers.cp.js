847d24c40c5f3dbea6b8053bd11e5a650e04e9f0
Robin Schreiber <robin.schreiber@me.com>
2022-01-27 15:02:28 +0100

    helpers: fix comments

--- studio/helpers.cp.js
+++ studio/helpers.cp.js
@@ -7,40 +7,45 @@ import { Color, pt } from 'lively.graphics';
 import { VerticalLayout, TilingLayout, Text, Label } from 'lively.morphic';
 import { rect } from 'lively.graphics/geometry-2d.js';
 
+/**
+ * A Bug Reporter.  Very simple: just bundles up the input fields and uses
+ * a POST call to report the bug and file a ticket.  No properties, just
+ * a read-only URL
+ * the protocol to report Lively bugs is in
+ * https://gitlab.com/engageLively/ticketingsystem/-/blob/master/README.md
+ * Sample cURL call is:
+ * curl -H "Content-Type: application/json" -d '{"user": "foo@bar.com", "file_path":"/dev/null", "message":"First App Engine Test"}' https://engagelively.wl.r.appspot.com/ticket
+ */
 export class BugReporterModel extends ViewModel {
-  /*
-  ** A Bug Reporter.  Very simple: just bundles up the input fields and uses
-  ** a POST call to report the bug and file a ticket.  No properties, just
-  ** a read-only URL
-  ** the protocol to report Lively bugs is in
-  ** https://gitlab.com/engageLively/ticketingsystem/-/blob/master/README.md
-  ** Sample cURL call is:
-  ** curl -H "Content-Type: application/json" -d '{"user": "foo@bar.com", "file_path":"/dev/null", "message":"First App Engine Test"}' https://engagelively.wl.r.appspot.com/ticket
-  */
-
-  // initialize the user and file_path fields with the values from the
-  // environment, if provided (non-null and length > 0)
-
+  /**
+   * initialize the user and file_path fields with the values from the
+   * environment, if provided (non-null and length > 0)
+   * @param { string } userName - The name of the current user.
+   * @param { string } filePath - The path to the dashboard file.
+   */
   init (userName, filePath) {
     this._initInputField_('user', userName);
     this._initInputField_('file_path', filePath);
   }
-
-  // initialize an input field with a value, if the value is non-null and
-  // its length is > 0.  For nulls or empty values, do nothing.
-
+  
+  /**
+   * Initialize an input field with a value, if the value is non-null and
+   * its length is > 0.  For nulls or empty values, do nothing.
+   * @param { string } inputFieldName - The name of the field to initialize.
+   * @param { string } value - The text string to be placed into the input field.
+   */
   _initInputField_ (inputFieldName, value) {
     if (value && value.length > 0) {
       this.getSubmorphNamed(inputFieldName).textString = value;
     }
   }
-
-  // report a bug.  Just collect the inputs, put dummies in for file_path
-  // and user if nothing is entered, ensure that there is a message, then
-  // post the request to post it.  IMPORTANT: once the request is
-  // posted, close this dialog to prevent accidental spamming.
-  // body = {user: "foo@galyleo.com", file_path:"/dev/galyleo", message:"first test from Galyleo"}
-  // message = ''
+  
+  /**
+   * Report a bug.  Just collect the inputs, put dummies in for file_path
+   * and user if nothing is entered, ensure that there is a message, then
+   * post the request to post it.  IMPORTANT: once the request is
+   * posted, close this dialog to prevent accidental spamming.
+   */
   reportBug () {
     const user = this.getSubmorphNamed('user').textString;
     const file_path = this.getSubmorphNamed('file_path').textString;
@@ -188,18 +193,18 @@ const BugReporter = component(GalyleoWindow, {
   })]
 });
 
+/**
+ * A very simple part that just consists of an input string and two buttons,
+ * where an URL is entered and then read and loaded.  The URL must reference
+ * a JSON structure which is a suitable argument to Google DataTable, see:
+ * https://developers.google.com/chart/interactive/docs/reference#constructor
+ * This structure can be created from the Python class gviz_api.DataTable
+ * with the call <table>.ToJSon(), where <table> is an instance of
+ * gviz_api.DataTable.  See:
+ * https://developers.google.com/chart/interactive/docs/dev/gviz_api_lib
+ * The cancel button just invokes this.remove() with a hardcoded connection
+ */
 export class TableLoaderModel extends ViewModel {
-  /* A very simple part that just consists of an input string and two buttons,
-  ** where an URL is entered and then read and loaded.  The URL must reference
-  ** a JSON structure which is a suitable argument to Google DataTable, see:
-  ** https://developers.google.com/chart/interactive/docs/reference#constructor
-  ** This structure can be created from the Python class gviz_api.DataTable
-  ** with the call <table>.ToJSon(), where <table> is an instance of
-  ** gviz_api.DataTable.  See:
-  ** https://developers.google.com/chart/interactive/docs/dev/gviz_api_lib
-  ** The cancel button just invokes this.remove() with a hardcoded connection
-  */
-
   static get properties () {
     return {
       bindings: {
@@ -212,24 +217,29 @@ export class TableLoaderModel extends ViewModel {
       }
     };
   }
-
-  // closes the prompt
+  
+  /**
+   * Closes the prompt
+   */
   close () {
     this.view.remove();
   }
-
-  // initialize this with the  dashboard to call back.  This is called by D
-  // ashboardControl.loadTable after this part has been created in that routine.
-
+  
+  /**
+   * Initialize this with the  dashboard to call back.  This is called by
+   * DashboardControl.loadTable after this part has been created in that routine.
+   * @param { Dashboard } dashboard - The dashboard to bind this table loader to.
+   */
   init (dashboard) {
     this.dashboard = dashboard;
   }
-
-  // Tell the dashboard to load the table from this URL.  The URL should be
-  // checked here first, to ensure it's at least a valid URL.  Once done,
-  // just close the dialog.  This is called from the Load URL button via a
-  // hardcoded connection.
-
+  
+  /**
+   * Tell the dashboard to load the table from this URL.  The URL should be
+   * checked here first, to ensure it's at least a valid URL.  Once done,
+   * just close the dialog.  This is called from the Load URL button via a
+   * hardcoded connection.
+   */
   loadURL () {
     const url = this.ui.url;
     if (url.input.length === 0) {
