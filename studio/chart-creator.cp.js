ce047b8bb12ba9bc7f3a8bff966c18cc5af38b2b
Robin Schreiber <robin.schreiber@me.com>
2022-03-02 12:09:03 +0100

    chart creator: fix documentation

--- studio/chart-creator.cp.js
+++ studio/chart-creator.cp.js
@@ -290,21 +290,21 @@ const ChartBuilder = component(GalyleoWindow, {
 });
 
 export class GoogleChartHolderMorph extends HTMLMorph {
-  /*
-  ** A morph which draws a Google Chart.  Very simple, because almost all
-  ** the work is done by the dashboard.  The central routine in this is drawChart(),
-  ** which just asks a ChartWrapper created by the Dashboard to draw the chart
-  ** in this object's DOM node .  See:
-  ** https://developers.google.com/chart/interactive/docs/reference#chartwrapperobject
-  ** for documentation on the ChartWrapper.
-  ** Properties:
-  ** 1. the chartDiv.  Just a property which encapsulates the DOM node.  This is
-  **    passed to the wrapper as the place to draw the chart
-  ** 2. defaultHTML: overrides the parent node property to ensure that this
-  **    DOM node has a referenceable ID.
-  ** Check to see if the wrapper should be made a property.  The only reason to do
-  ** this would be if the thing doesn't redraw properly on load.
-  */
+  /**
+   * A morph which draws a Google Chart.  Very simple, because almost all
+   * the work is done by the dashboard.  The central routine in this is drawChart(),
+   * which just asks a ChartWrapper created by the Dashboard to draw the chart
+   * in this object's DOM node .  See:
+   * https://developers.google.com/chart/interactive/docs/reference#chartwrapperobject
+   * for documentation on the ChartWrapper.
+   * Properties:
+   * 1. the chartDiv.  Just a property which encapsulates the DOM node.  This is
+   *    passed to the wrapper as the place to draw the chart
+   * 2. defaultHTML: overrides the parent node property to ensure that this
+   *    DOM node has a referenceable ID.
+   * Check to see if the wrapper should be made a property.  The only reason to do
+   * this would be if the thing doesn't redraw properly on load.
+   */
   static get properties () {
     return {
       chartDiv: { // The node where the chart lives.
@@ -321,20 +321,23 @@ export class GoogleChartHolderMorph extends HTMLMorph {
       }
     };
   }
-
-  // An indicator that this morph is a chart, which the dashboard uses
-  // to manipulate and organize the chart morphs in the dashboard.
-
+  
+  /**
+   * An indicator that this morph is a chart, which the dashboard uses
+   * to manipulate and organize the chart morphs in the dashboard.
+   */
   get isChart () {
     return true;
   }
-
-  // Initialize.  Just set the morph name to the name of the chart on the
-  // dashboard, a technique the dashboard uses to keep track of which charts
-  // belong to which morphs.  Also initialize the dashboard instance variable and
-  // set the html to an initial value -- drawChart overwrites the HTML.  Called
-  // by dashboard.__getChartMorph__()
-
+  
+  /**
+   * Initialize. Just set the morph name to the name of the chart on the
+   * dashboard, a technique the dashboard uses to keep track of which charts
+   * belong to which morphs.  Also initialize the dashboard instance variable and
+   * set the html to an initial value -- drawChart overwrites the HTML.  Called
+   * by `dashboard._getChartMorph()`
+   * @param { string } chartName - The name of the chart.
+   */
   init (chartName) {
     this.name = chartName;
     this.html = this.defaultHTML;
@@ -342,36 +345,41 @@ export class GoogleChartHolderMorph extends HTMLMorph {
     this.requestRedraw();
     this.getSubmorphNamed('resizer').bottomRight = this.innerBounds().bottomRight();
   }
-
-  // Redraw, using the stored wrapper if any.  This is hardwired to this.extent, so
-  // when the shape of the morph changes the chart is redrawn.  The default
-  // options for charts include {width: "100%", height: "100%"}, so the wrapper
-  // code adjusts the chart size to exactly fill the morph.  The effect of all of
-  // this is that the chart seamlessly resizes when the morph is resized.
-  // This is why we cleaned up any lingering this.wrapper on init, and just to
-  // make sure we don't get bogus calls we only draw when we have a stored wrapper
-
+  
+  /**
+   * Redraw, using the stored wrapper if any.  This is hardwired to this.extent, so
+   * when the shape of the morph changes the chart is redrawn.  The default
+   * options for charts include {width: "100%", height: "100%"}, so the wrapper
+   * code adjusts the chart size to exactly fill the morph.  The effect of all of
+   * this is that the chart seamlessly resizes when the morph is resized.
+   * This is why we cleaned up any lingering this.wrapper on init, and just to
+   * make sure we don't get bogus calls we only draw when we have a stored wrapper
+   */
   async requestRedraw () {
     if (this.wrapper) {
       await this.whenRendered(); // ensure width is propagated
       this.drawChart(this.wrapper);
     }
   }
-
-  // Actually draw the chart.  We first make sure that width and height are set
-  // to 100% -- we do this in ChartBuilder.__makeOptions__ as well, so this is
-  // suspenders-and-belt, and then we just tell the wrapper to draw the chart
-  // in this div.
-
+  
+  /**
+   * Actually draw the chart.  We first make sure that width and height are set
+   * to 100% -- we do this in `ChartBuilder.__makeOptions__` as well, so this is
+   * suspenders-and-belt, and then we just tell the wrapper to draw the chart
+   * in this div.
+   * @param { object } wrapper - The google chart wrapper.
+   */
   drawChart (wrapper) {
     wrapper.setOption('width', '100%');
     wrapper.setOption('height', '100%');
     wrapper.draw(this.chartDiv);
     this.wrapper = wrapper;
   }
-
-  // Resize the chart in response to a drag event
-
+  
+  /**
+   * Resize the chart in response to a drag event
+   * @param { Event } evt - The drag event.
+   */
   resizeChart (evt) {
     this.resizeBy(evt.state.dragDelta);
     this.requestRedraw();
