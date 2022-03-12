d184a851f8161a9cddf3961a0798f9110b152927
Robin Schreiber <robin.schreiber@me.com>
2022-03-12 10:46:13 +0100

    continue japanese translation

--- studio/int/jp/side-bar.cp.js
+++ studio/int/jp/side-bar.cp.js
@@ -2,39 +2,229 @@ import { GalyleoSideBar } from '../../side-bar.cp.js';
 import { component } from 'lively.morphic/components/core.js';
 import { pt } from 'lively.graphics';
 
-// GalyleoSideBarJapanese.openInWorld()
+// GalyleoSideBarJp.openInWorld()
 const GalyleoSideBarJp = component(GalyleoSideBar, {
   name: 'galyleo/side bar/jp',
   extent: pt(324.8, 668),
-  submorphs: [{
-    name: 'tab switcher',
-    extent: pt(364, 32.7),
-    submorphs: [{
-      name: 'tables tab',
+  submorphs: [
+    {
+      name: 'controls',
       submorphs: [{
-        name: 'tab label',
-        textAndAttributes: ['テーブル', null]
+        name: 'tab switcher',
+        extent: pt(364, 32.7),
+        submorphs: [{
+          name: 'tables tab',
+          submorphs: [{
+            name: 'tab label',
+            textAndAttributes: ['テーブル', null]
+          }]
+        }, {
+          name: 'filters tab',
+          submorphs: [{
+            name: 'tab label',
+            textAndAttributes: ['フィルル', null]
+          }]
+        }, {
+          name: 'charts tab',
+          submorphs: [{
+            name: 'tab label',
+            textAndAttributes: ['チャート', null]
+          }]
+        }, {
+          name: 'views tab',
+          submorphs: [{
+            name: 'tab label',
+            textAndAttributes: ['ビュウー', null]
+          }]
+        }]
+      },
+      {
+        name: 'control container',
+        submorphs: [
+          {
+            name: 'chart control',
+            visible: true,
+            submorphs: [
+              {
+                name: 'v wrapper',
+                submorphs: [
+                  { name: 'control label', textAndAttributes: ['チャート', null] }
+                ]
+              }, {
+                name: 'add button',
+                extent: pt(157, 35),
+                submorphs: [
+                  { name: 'label', textAndAttributes: ['チャートを追加', null] }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'view control',
+            visible: true,
+            submorphs: [
+              {
+                name: 'v wrapper',
+                submorphs: [
+                  { name: 'control label', textAndAttributes: ['ビュー', null] }
+                ]
+              }, {
+                name: 'add button',
+                extent: pt(130, 35),
+                submorphs: [
+                  { name: 'label', textAndAttributes: ['ビューを追加', null] }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'tables control',
+            visible: true,
+            submorphs: [
+              {
+                name: 'v wrapper',
+                submorphs: [
+                  { name: 'control label', textAndAttributes: ['テーブル', null] }
+                ]
+              }, {
+                name: 'add button',
+                extent: pt(157, 35),
+                submorphs: [
+                  { name: 'label', textAndAttributes: ['テーブルを追加', null] }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'filters control',
+            submorphs: [
+              {
+                name: 'v wrapper',
+                submorphs: [
+                  { name: 'control label', textAndAttributes: ['フィルタとウィジェット', null] }
+                ]
+              }, {
+                name: 'add button',
+                extent: pt(157, 35),
+                submorphs: [
+                  { name: 'label', textAndAttributes: ['フィルタを追加', null] }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'style control',
+        submorphs: [
+          {
+            name: 'background control',
+            submorphs: [
+              {
+                name: 'h floater',
+                submorphs: [
+                  {
+                    name: 'section headline',
+                    textAndAttributes: ['背景', null]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'text control',
+            submorphs: [
+              {
+                name: 'h floater',
+                submorphs: [
+                  {
+                    name: 'section headline',
+                    textAndAttributes: ['リッチテキスト', null]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'layout control',
+            submorphs: [
+              {
+                name: 'h floater',
+                submorphs: [
+                  {
+                    name: 'section headline',
+                    textAndAttributes: ['レイアウト', null]
+                  }
+                ]
+              },
+              {
+                name: 'wrap submorphs checkbox',
+                submorphs: [
+                  { name: 'prop label', textAndAttributes: ['副要素の折り返し', null] }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'alignment control',
+            submorphs: [
+              {
+                name: 'h floater',
+                submorphs: [
+                  {
+                    name: 'section headline',
+                    textAndAttributes: ['制約事項', null]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'fill control',
+            submorphs: [
+              {
+                name: 'h floater',
+                submorphs: [
+                  {
+                    name: 'section headline',
+                    textAndAttributes: ['フィル', null]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'border control',
+            submorphs: [
+              {
+                name: 'h floater',
+                submorphs: [
+                  {
+                    name: 'section headline',
+                    textAndAttributes: ['枠線', null]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'effects control',
+            submorphs: [
+              {
+                name: 'h floater',
+                submorphs: [
+                  {
+                    name: 'section headline',
+                    textAndAttributes: ['カスタム スタイル', null]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       }]
-    }, {
-      name: 'filters tab',
-      submorphs: [{
-        name: 'tab label',
-        textAndAttributes: ['フィルル', null]
-      }]
-    }, {
-      name: 'charts tab',
-      submorphs: [{
-        name: 'tab label',
-        textAndAttributes: ['チャート', null]
-      }]
-    }, {
-      name: 'views tab',
-      submorphs: [{
-        name: 'tab label',
-        textAndAttributes: ['ビュウー', null]
-      }]
-    }]
-  }]
+    }
+  ]
 });
 
 export { GalyleoSideBarJp };
