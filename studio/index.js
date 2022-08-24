049fae92b2ba2af16e2af109750b867b9deba6ad
Robin Schreiber <robin.schreiber@me.com>
2022-08-24 22:38:12 +0200

    implement index.js build target

---
+++ studio/index.js
@@ -0,0 +1,24 @@
+import { part } from 'lively.morphic';
+import { LivelyWorld } from 'lively.ide/world.js';
+
+export async function main () {
+  const { GalyleoDashboardStudio } = await System.import('galyleo-dashboard/studio/int/en');
+  const dashboard = part(GalyleoDashboardStudio);
+  dashboard.respondsToVisibleWindow = true;
+  $world.addMorph(dashboard);
+  dashboard.relayout();
+}
+
+export const TITLE = 'Galyleo Dashboard Studio';
+
+export const WORLD_CLASS = LivelyWorld;
+
+export const EXCLUDED_MODULES = [
+  'pouchdb',
+  'pouchdb-adapter-mem',
+  'rollup',
+  'lively.freezer',
+  'lively.modules',
+  'lively.storage',
+  'lively.user'
+];
