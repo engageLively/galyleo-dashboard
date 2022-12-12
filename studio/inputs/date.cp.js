f41f9b8ed72a0aa66648bdaa7a2adecad11ca008
Rick McGeer <rick@mcgeer.com>
2022-12-12 15:17:05 -0800

    moved ensureFont to a reasonable spot

--- studio/inputs/date.cp.js
+++ studio/inputs/date.cp.js
@@ -1,13 +1,10 @@
 import { signal } from 'lively.bindings/index.js';
-import { component, ViewModel, part, ensureFont } from 'lively.morphic/components/core.js';
+import { component, ViewModel, part } from 'lively.morphic/components/core.js';
 import { InputLine, Morph, TilingLayout, Icon, ShadowObject, Label } from 'lively.morphic';
 import { Color, rect, pt } from 'lively.graphics/index.js';
 
 import { arr, date } from 'lively.lang/index.js';
 
-ensureFont({
-  Barlow: 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-});
 
 export class GalyleoDate extends Label {
   /*
--- studio/shared.cp.js
+++ studio/shared.cp.js
@@ -1,6 +1,6 @@
 import { component, without, add, ViewModel, part } from 'lively.morphic/components/core.js';
 import { pt, LinearGradient, Color, rect } from 'lively.graphics';
-import { TilingLayout, Icon, ShadowObject, Image, Label, Morph, Text } from 'lively.morphic';
+import { TilingLayout, ensureFont, Icon, ShadowObject, Image, Label, Morph, Text } from 'lively.morphic';
 import { arr, obj, num } from 'lively.lang';
 import { MorphList, DropDownListModel } from 'lively.components';
 import { connect, noUpdate } from 'lively.bindings';
@@ -10,6 +10,10 @@ import { NumberInputDark, TextInput, PropertyLabelHovered, PropertyLabelActive, 
 import { ColorInput } from 'lively.ide/styling/color-picker.cp.js';
 import { ConfirmPromptModel } from 'lively.components/prompts.cp.js';
 import { ButtonModel } from 'lively.components/buttons.js';
+
+ensureFont({
+  Barlow: 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+});
 
 export class SelectableEntryModel extends ViewModel {
   static wrap (entryName, opts) {
