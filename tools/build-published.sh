1794349a1eb9d73dbf5942b04d738ac81885f676
Robin Schreiber <robin.schreiber@me.com>
2023-07-03 23:11:12 +0200

    fix published build script

--- tools/build-published.sh
+++ tools/build-published.sh
@@ -2,4 +2,4 @@
 . ../scripts/lively-next-flatn-env.sh
 lively_next_flatn_env "$(dirname "$(pwd)")"
 export FLATN_DEV_PACKAGE_DIRS=$FLATN_DEV_PACKAGE_DIRS:$(pwd);
-node --inspect --no-warnings --experimental-import-meta-resolve --experimental-loader ../flatn/resolver.mjs ./tools/build-published.mjs
+node --inspect --no-warnings --experimental-import-meta-resolve --experimental-loader ../../flatn/resolver.mjs ./tools/build-published.mjs
