c18956acb00d9378e4818cd26d10d5f5aeeb9ffd
Robin Schreiber <robin.schreiber@me.com>
2023-07-04 10:32:57 +0200

    remove ensureFont call

--- studio/shared.cp.js
+++ studio/shared.cp.js
@@ -1,6 +1,6 @@
 import { component, without, add, ViewModel, part } from 'lively.morphic/components/core.js';
 import { pt, LinearGradient, Color, rect } from 'lively.graphics';
-import { TilingLayout, ensureFont, Icon, ShadowObject, Image, Label, Morph, Text } from 'lively.morphic';
+import { TilingLayout, Icon, ShadowObject, Image, Label, Morph, Text } from 'lively.morphic';
 import { arr, obj, num } from 'lively.lang';
 import { MorphList, DropDownListModel } from 'lively.components';
 import { connect, noUpdate } from 'lively.bindings';
@@ -13,10 +13,6 @@ import { ButtonModel } from 'lively.components/buttons.js';
 import { projectAsset } from 'lively.project/helpers.js';
 
 const galyleoFont = 'Sans-Serif'; // formerly 'Barlow', changed due to failure to import
-
-ensureFont({
-  Barlow: 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-});
 
 export class SelectableEntryModel extends ViewModel {
   static wrap (entryName, opts) {
